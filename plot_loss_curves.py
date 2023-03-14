66d72b8fbb8ea5628c097ac87a5e7f0c0eecee94
kvalaroutsos <66298082+kvalaroutsos@users.noreply.github.com>
2023-03-14 15:03:02 +0200

    Create plot_loss_curves.py

---
+++ plot_loss_curves.py
@@ -0,0 +1,25 @@
+# Plot the validation and training curves separately
+def plot_loss_curves(history):
+
+  loss=history.history['loss']
+  val_loss=history.history['val_loss']
+
+  accuracy=history.history['accuracy']
+  val_accuracy=history.history['val_accuracy']
+
+  epochs=range(len(history.history['loss']))
+
+  #Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('loss')
+  plt.xlabel('epochs')
+  plt.legend()
+
+    #Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('accuracy')
+  plt.xlabel('epochs')
+  plt.legend()
